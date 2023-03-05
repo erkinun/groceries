676526cf7ca2ecd6200b84c3206d49755b7c3b32
erkinun <erkinun@gmail.com>
2023-03-05 16:02:38 +0000

    profile page introduced

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // TODO remove css and replace with tailwind
 import { Dashboard } from './pages/Dashboard';
+import ErrorPage from './pages/ErrorPage';
 import { Login } from './pages/Login';
+import { Profile } from './pages/Profile';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <ErrorPage />,
     element: <Login />,
   },
   {
     path: '/dashboard',
     element: <Dashboard />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
   },
 ]);
 
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth } from '../firebase';
+import { updateUserName, useProfile } from '../queries/user';
+
+export function Profile() {
+  const [user, loading, error] = useAuthState(auth);
+  const profile = useProfile(user?.uid ?? '');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSave = () => {
+    updateUserName(user?.uid ?? '', inputRef.current?.value ?? '');
+  };
+
+  return (
+    <div>
+      <div>Hi {profile.userName ?? user?.displayName}!</div>
+      <div>
+        <h2>Set your user name</h2>
+        <input
+          ref={inputRef}
+          type="text"
+          defaultValue={user?.displayName ?? ''}
+        />
+        <button onClick={handleSave}>Save</button>
+      </div>
+    </div>
+  );
+}
