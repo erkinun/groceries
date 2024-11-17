6db07da1ccfcd25b30ec624eb0fe046b09eba321
Erkin Ünlü <erkinun@gmail.com>
2024-11-17 14:38:24 +0000

    sentry fix try 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "erkin-unlu",
-    project: "javascript-react"
-  })],
-
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true, // Source map generation must be turned on
+  },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'erkin-unlu',
+      project: 'groceries',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
+});
